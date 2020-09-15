f8921be09bda3c4058831d1cdc45b6eabc7d6ac3
siransfe <hclee8907@gmail.com>
2020-09-15 10:26:49 +0900

    Create Model, Superuser
    create superuser
    make models

--- testboard/board/admin.py
+++ testboard/board/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+# models.py 에 작성한 class 등록 
+
--- testboard/board/models.py
+++ testboard/board/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from model_utils.fields import StatusField
+from model_utils import Choices
 
 # Create your models here.
+
+class board (models.Model):
+    title = models.CharField(max_length = 50)
+    contents = models.CharField(max_length = 200) 
+    created = models.DateTimeField('date_created')
+    modified = models.DateTimeField('date_modified') 
+
+#class status (models.Model):
+
+#createsuperuser
+
+'''
+model {
+  title
+  contents
+  created
+  modified
+}
+'''
