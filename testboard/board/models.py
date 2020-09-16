f6785955d82039498f06d29924a1bb2c63e51979
siransfe <hclee8907@gmail.com>
2020-09-16 11:09:15 +0900

    Add draft/published status
    finished migratation

--- testboard/board/models.py
+++ testboard/board/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+from model_utils.fields import StatusField
+from model_utils import Choices
 
 class board (TimeStampedModel):
+    STATUS = Choices('draft', 'published')
+    status = StatusField()
     title = models.CharField(max_length = 50)
-    contents = models.TextField()
+    contents = models.TextField() 
+    
