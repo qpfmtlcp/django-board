f3f34d412c3b56bda77b6d48d67eb7bf74f308fb
siransfe <hclee8907@gmail.com>
2020-09-16 11:11:12 +0900

    Add admin list
    status is added in admin list

--- testboard/board/admin.py
+++ testboard/board/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import board
 
 class boardAdmin(admin.ModelAdmin):
-    list_display = ('title', 'contents', 'created','modified')
+    list_display = ('title', 'status', 'contents', 'created','modified')
 
 admin.site.register(board, boardAdmin)
