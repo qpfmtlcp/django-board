ebf270b680a2163c817f6dd40489c3deeeb44992
siransfe <hclee8907@gmail.com>
2020-09-18 07:04:51 +0900

    Add variables in update method

--- testboard/board/serializer.py
+++ testboard/board/serializer.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 
 
 class HistorySerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+
     class Meta:
         model = History
-        fields = ['board', 'created']
+        fields = ['user', 'created']
 
 
 class BoardSerializer(serializers.ModelSerializer):
@@ -25,6 +27,15 @@ class BoardSerializer(serializers.ModelSerializer):
             'history',
             'user',
         ]
+
+    def update(self, instance, validated_data):
+        username = validated_data.pop("user")
+        currentUser = User.objects.all()
+        for name in currentUser:
+            if name.username == username:
+                print(name.id)
+                History.objects.create(user=currentUser)
+        return instance
 
 
 class UserSerializer(serializers.ModelSerializer):
