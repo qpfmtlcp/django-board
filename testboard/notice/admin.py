4c6d3976dd273f37148616cc1d2da29c2689d597
siransfe <hclee8907@gmail.com>
2020-09-23 01:20:05 +0900

    Add tag Model

--- testboard/notice/admin.py
+++ testboard/notice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import NoticeBoard
+
+
+class NoticeBoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'contents')
+
 
 # Register your models here.
+admin.site.register(NoticeBoard, NoticeBoardAdmin)
--- testboard/notice/models.py
+++ testboard/notice/models.py
@@ -15,3 +15,16 @@ class NoticeBoard(TimeStampedModel, StatusModel):
     owner = models.ForeignKey(
         'auth.User', related_name='snippets', on_delete=models.CASCADE
     )
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    # unique는 해당 필드가 테이블에서 유니크하게 저장됨. 중복되는 값이 있으면 오류 발생
+    def __str__(self):
+        return self.name
+
+
+# 태그를 링크처리. Charfiled로 받고 링크를 클릭하면 해당 링크가 포함된 post list 검색결과 나와야 함.
+# custom template filter를 사용하고 many to many 를 활용해얗됨.
+# notice와 tag모델은 many to many
