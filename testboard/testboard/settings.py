b79ecc8d36252bd566e17728b9ee5c88a28e3997
siransfe <hclee8907@gmail.com>
2020-09-24 13:23:28 +0900

    Modify debug status

--- testboard/testboard/settings.py
+++ testboard/testboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vh__^&j8pomf8*864^$90ha2wz)j@9+bbh8*=y5bb%x*^mmiu4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
