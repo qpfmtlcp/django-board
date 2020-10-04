6527daa30e8a13d66ff7e1d978d70e331840f81b
siransfe <hclee8907@gmail.com>
2020-10-05 00:47:02 +0900

    Add Tag class in models.py

--- testboard/board/models.py
+++ testboard/board/models.py
@@ -5,21 +5,29 @@ from model_utils.models import TimeStampedModel, StatusModel
 from model_utils import Choices
 
 
-class Board(TimeStampedModel, StatusModel):
-    STATUS = Choices('draft', 'published')
+class Tag(models.Model):
+    tagname = models.CharField(max_length=100)
 
-    title = models.CharField(max_length=50)
-    contents = models.TextField()
-    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True, upload_to='image/')
+    def __str__(self):
+        return "#" + self.tagname
+    
+class Board(TimeStampedModel, StatusModel):
+    STATUS  = Choices('draft', 'published')
+
+    title   = models.CharField(max_length=50)
+    contents= models.TextField()
+    owner   = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    image   = models.ImageField(null=True, blank=True, upload_to='image/')
+    tag     = models.ManyToManyField(Tag, related_name='board')
 
 
 
 class History(TimeStampedModel):
-    board = models.ForeignKey(
+    board   = models.ForeignKey(
         Board,
         related_name='history',
-        on_delete=models.CASCADE,
-        null=False,
+        on_delete   =models.CASCADE,
+        null        =False,
     )
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    user    = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    
